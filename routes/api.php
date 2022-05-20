629acafdc87471fa4b8b98ab1b8e87e31e333ac8
alexisperez <aleperez.py@gmail.com>
2022-05-20 15:21:47 -0400

    #3 Controlador de Articulos y ruta

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{id}', [ArticleController::class, 'show']);
+Route::post('articles', [ArticleController::class, 'store']);
+Route::put('articles/{id}', [ArticleController::class, 'update']);
+Route::delete('articles/{id}', [ArticleController::class, 'delete']);
